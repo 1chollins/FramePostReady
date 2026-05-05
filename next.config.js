a0a9e4b934f5b84d33aae93f14837b706f21310f
Colby Hollins <contact@frameandformstudio.com>
2026-05-05 09:00:17 -0400

    Disable ESLint during Vercel builds

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     serverComponentsExternalPackages: ['apify-client', 'proxy-agent', 'got-scraping'],
   },
